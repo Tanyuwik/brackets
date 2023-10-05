4dee4eac9e6c421737d8d43bcc13c72bd0fcd718
Tetiana <tanyuwik777@gmail.com>
2023-10-05 20:11:09 +0200

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const bracketPairs = Object.fromEntries(bracketsConfig);
+
+  for (const char of str) {
+    if (char === bracketPairs[stack[stack.length - 1]]) {
+      stack.pop();
+    } else if (bracketPairs[char]) {
+      stack.push(char);
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
+
